d557bb5f120b27030c1cd51b10ca41e9236d8bb8
AmurTigro <martiohusein27@gmail.com>
2024-08-12 14:53:25 +0700

    Configure deployment using vercel and aiven

---
+++ api/index.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Contracts\Http\Kernel;
+
+require __DIR__ . '/../public/index.php';
+// require __DIR__ . '/../vendor/autoload.php';
+
+// $app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// $kernel  = $app->make(Kernel::class);
+
+// $response = $kernel->handle(
+//   $request = Illuminate\Http\Request::capture()
+// );
+
+// $response->send();
+// $kernel->terminate($request, $response);
+
