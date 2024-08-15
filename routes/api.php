dfa8ef1db3b5cd5821c9d8b03a78c926570b8413
AmurTigro <martiohusein27@gmail.com>
2024-08-15 14:35:35 +0700

    Fix missed place

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::apiResource('employees', EmployeeController::class);
 Route::middleware(['auth:sanctum'])->group(function () {
     //* Resourceful route for employees
+    Route::apiResource('employees', EmployeeController::class);
 
     //* Custom routes
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
